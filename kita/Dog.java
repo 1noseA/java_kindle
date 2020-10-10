912b62b30a8ecda736880afa747498a7ab595104
1noseA <1noseA2L@gmail.com>
2020-10-10 20:49:42 +0900

    練習問題7-3

--- kita/Dog.java
+++ kita/Dog.java
@@ -2,12 +2,18 @@ package kita;
 
 class Dog {
   private String Name;
+  private int Age;
 
   public void SetName(String nm) {
     Name = nm;
   }
 
+  public void SetAge(int age) {
+    Age = age;
+  }
+
   public void ShowProfile() {
     System.out.println("名前は" + Name + "です。");
+    System.out.println("年齢は" + Age + "才です。");
   }
 }
--- kita/Exercise.java
+++ kita/Exercise.java
@@ -2,8 +2,13 @@ package kita;
 
 public class Exercise {
   public static void main(String[] args) {
-    Dog MyDog = new Dog();
-    MyDog.SetName("ぽち");
-    MyDog.ShowProfile();
+    Dog Dog1 = new Dog();
+    Dog Dog2 = new Dog();
+    Dog1.SetName("ぽち");
+    Dog1.SetAge(3);
+    Dog1.SetName("たろう");
+    Dog2.SetAge(5);
+    Dog1.ShowProfile();
+    Dog2.ShowProfile();
   }
 }
